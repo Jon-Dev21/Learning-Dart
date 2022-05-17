e2bc401d5cc6f357925b345f47442075ddb6ba24
Jon-Dev21 <jonacevedev96@gmail.com>
2022-05-17 19:53:39 -0400

    Created new dart file to explain all Data Type Operators
    For now, arithmetic operators only explained

---
+++ DataType-Operators.dart
@@ -0,0 +1,30 @@
+/**
+ * Learning about data type operators.
+ */
+
+void main() {
+/**
+ * Arithmetic Operators
+ */
+
+// + operator: adds 2 values
+var sum = 10+10;
+print("10 + 10 = $sum");
+
+// - operator: subtracts 2 values
+var sub = 10-10;
+print("10 - 10 = $sub");
+
+// * operator: multiplies 2 values
+var mult = 10*10;
+print("10 * 10 = $mult");
+
+// - operator: divides 2 values
+var div = 9/2;
+print("9 / 2 = $div");
+
+// ~/ operator: Integer division of 2 values(result is always an integer).
+var intDiv = 9~/2;
+print("9 ~/ 2 = $intDiv");
+
+}
